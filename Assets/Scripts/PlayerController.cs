71c2a8f9914a9107dc5f9a81f1379fadcbd87a3f
Md. Atiqul Islam <6184486+airOfBengal@users.noreply.github.com>
2026-03-06 21:50:36 +0600

    add score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,34 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public float thurstForce = 1f;
     Rigidbody2D rb;
+    private float elapsedTime = 0f;
+    private float score = 0f;
+    public float scoreMultiplier = 10f;
+    public UIDocument uiDocument;
+    private Label scoreLabel;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scoreLabel = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Mouse.current.leftButton.isPressed)
+        UpdateScore();
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        if (Mouse.current.leftButton.isPressed)
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             Vector2 direction = (mousePos - transform.position).normalized;
@@ -23,6 +36,13 @@ public class PlayerController : MonoBehaviour
 
             rb.AddForce(direction * thurstForce);
         }
+    }
+
+    private void UpdateScore()
+    {
+        elapsedTime += Time.deltaTime;
+        score = Mathf.FloorToInt(elapsedTime * scoreMultiplier); // Update score based on elapsed time
+        scoreLabel.text = $"Score: {score}"; // Update the score label in the UI
     }
 
     void OnCollisionEnter2D(Collision2D collision)
